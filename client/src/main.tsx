75ae98c95562103b493efe3ddba6b8648fea5ba0
Aung Myat Moe <aungmyatmoe834@gmail.com>
2023-01-24 14:39:55 +0630

    feat: add rtk for state management

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -6,11 +6,15 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import App from './App'
 import CssBaseline from '@mui/material/CssBaseline';
+import {Provider} from "react-redux";
+import {store} from "./store";
 
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <CssBaseline/>
-        <App/>
+        <Provider store={store}>
+            <CssBaseline/>
+            <App/>
+        </Provider>
     </React.StrictMode>,
 )
